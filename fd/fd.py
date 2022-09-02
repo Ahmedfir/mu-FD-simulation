8d364f2ec687ef3060acb95fbb47c83a3f89cf47
ahmed khanfir <ahmed.khanfir.89@gmail.com>
2022-09-02 17:39:04 +0200

    FD-2 spread fd by all steps

--- fd/fd.py
+++ fd/fd.py
@@ -1,13 +1,23 @@
 import copy
 import random
+from enum import Enum
+from os import makedirs
+from os.path import join, isdir, isfile
 
-from typing import Set, List
+from typing import Set, List, Dict
+
+from utils.pickle_utils import load_zipped_pickle, save_zipped_pickle
+
+DEFAULT_EXCLUDED_TESTS = ["''", "nan", ""]
+
+
+# todo refactor: most of code is duplicated considering mutants or tests as cost
 
 
 class Mutant:
 
-    def __init__(self, failing_tests: Set[str]):
-        self.failing_tests = failing_tests
+    def __init__(self, failing_tests: Set[str], exclude=DEFAULT_EXCLUDED_TESTS):
+        self.failing_tests = {t for t in failing_tests if t not in exclude}
 
     def killed(self) -> bool:
         return self.failing_tests is not None and len(self.failing_tests) > 0
@@ -23,6 +33,21 @@ class TargetCost:
         self.achieved = True
         self.mutants_analysed = mutants_analysed
         self.tests_written = tests_written
+
+    def _fd_at(self, effort, finding_effort) -> float:
+        if self.achieved:
+            if effort >= finding_effort:
+                return 1.0
+            else:
+                return float(effort) / float(finding_effort)
+        else:
+            return 0.0
+
+    def fd_mutants_at(self, effort):
+        return self._fd_at(effort, self.mutants_analysed)
+
+    def fd_tests_at(self, effort):
+        return self._fd_at(effort, self.tests_written)
 
 
 class SimulationResults:
@@ -46,15 +71,17 @@ class SimulationResults:
     def killed_all_mutants(self):
         return self.kill_all_mutants_cost.achieved
 
+    def fd_mutants_at(self, effort) -> float:
+        return self.bug_finding_cost.fd_mutants_at(effort)
+
+    def fd_tests_at(self, effort):
+        return self.bug_finding_cost.fd_tests_at(effort)
+
 
 class Practitioner:
 
     def __init__(self, mutants: List[Mutant], ranked: bool = False):
         self.mutants = copy.deepcopy(mutants)
-        killed_mutants = {m for m in mutants if m.killed()}
-        all_failing_tests = {t for m in killed_mutants for t in m.failing_tests}
-        self.test_killed_mutants_map = {t: {m for m in killed_mutants if t in m.failing_tests}
-                                        for t in all_failing_tests}
         self.ranked = ranked
         self.analysed_mutants = 0
         self.written_tests = 0
@@ -68,8 +95,7 @@ class Practitioner:
     def write_test_to_kill_mutant(self, mutant):
         self.written_tests = self.written_tests + 1
         test = list(mutant.failing_tests)[random.choice(range(0, len(mutant.failing_tests)))]
-        self.mutants = [m for m in self.mutants if m not in self.test_killed_mutants_map[test]]
-        del self.test_killed_mutants_map[test]
+        self.mutants = [m for m in self.mutants if test not in m.failing_tests]
         assert mutant not in self.mutants
         return test
 
@@ -103,11 +129,98 @@ class Practitioner:
 
 class Simulation:
 
-    def __init__(self, failing_tests: Set[str], mutants, ranked: bool = False, repeat=100):
+    def __init__(self, failing_tests: Set[str]):
         self.failing_tests = failing_tests
-        self.ranked = ranked  # default is random.
-        self.mutants = mutants
-        self.repeat = repeat
 
-    def process(self):
-        return [Practitioner(self.mutants, self.ranked).simulate(self.failing_tests) for _ in range(0, self.repeat)]
+    def process_ranked_pools(self, mutant_pools: List[List[Mutant]], repeat=100):
+        result = []
+        for _ in range(0, repeat):
+            mutants = []
+            for m_pool in mutant_pools:
+                random.shuffle(m_pool)
+                mutants.extend(m_pool)
+            result.append(self.process(mutants, True, repeat=1))
+        return result
+
+    def process(self, mutants: List[Mutant], ranked: bool = False, repeat=100):  # default is random.
+        return [Practitioner(mutants, ranked).simulate(self.failing_tests) for _ in range(0, repeat)]
+
+
+class EffortMetrics(Enum):
+    M = "mutants_analaysed"
+    T = "tests_written"
+
+
+class SimulationsArray:
+    def __init__(self, simulations: List[SimulationResults]):
+        self.simulations = simulations
+
+    def max_cost(self, metric: EffortMetrics):
+        if EffortMetrics.M == metric:
+            return max([res.analyse_all_mutants.mutants_analysed for res in self.simulations])
+        elif EffortMetrics.T == metric:
+            return max([res.analyse_all_mutants.tests_written for res in self.simulations])
+        else:
+            raise Exception("{0} : metric not supported!")
+
+    def mean_fd_at(self, effort, metric: EffortMetrics) -> float:
+        if EffortMetrics.M == metric:
+            return sum([sa.fd_mutants_at(effort) for sa in self.simulations]) / float(len(self.simulations))
+        elif EffortMetrics.T == metric:
+            return sum([sa.fd_tests_at(effort) for sa in self.simulations]) / float(len(self.simulations))
+        else:
+            raise Exception("{0} : metric not supported!")
+
+
+class BugResults:
+
+    def __init__(self, bug_name: str, tools_results: Dict[str, SimulationsArray]):
+        self.bug_name = bug_name
+        self.tools_results: Dict[str, SimulationsArray] = tools_results
+
+    def fd_by_cost(self, metric: EffortMetrics, intermediate_dir, max_cost_min_tool=True, max_steps=100) -> Dict[
+        str, Dict[float, float]]:
+        pickle_file = None
+        # if disk caching is enabled, try to reload results form cache.
+        if intermediate_dir is not None:
+            pickle_dir = join(intermediate_dir, metric.name, str(max_steps), 'zoomed' if max_cost_min_tool else 'all')
+            if not isdir(pickle_dir):
+                makedirs(pickle_dir)
+            pickle_file = join(pickle_dir, self.bug_name + '_' + '_'.join(self.tools_results.keys()) + '.pickle')
+            if isfile(pickle_file):
+                return load_zipped_pickle(pickle_file)
+
+        # tool: [effort:fd]
+        result: Dict[str, Dict[float, float]] = dict()
+        all_max_costs = [sa.max_cost(metric) for sa in self.tools_results.values()]
+        if max_cost_min_tool:
+            max_cost = min(all_max_costs)
+            assert max_cost > 0, '{0} : one of the tools has no mutants {1}'.format(self.bug_name,
+                                                                                    self.tools_results.keys())
+            steps = max_cost if max_steps > max_cost else max_steps
+            for effort_step in range(0, max_cost, int(max_cost / steps)):
+                normalised_effort_step = effort_step * 100.0 / max_cost
+                for tool in self.tools_results:
+                    if tool in result:
+                        result[tool][normalised_effort_step] = self.tools_results[tool].mean_fd_at(effort_step, metric)
+                    else:
+                        result[tool] = {normalised_effort_step: self.tools_results[tool].mean_fd_at(effort_step, metric)}
+            for t in result:
+                assert max_cost in result[t].keys()
+        else:
+            max_cost = max(all_max_costs)
+            all_max_costs.sort()
+            intervals_steps = (0 if i == 0 else all_max_costs[i - 1], m, int(m / max_steps) if m > max_steps else 1 for
+                               i, m in enumerate(all_max_costs))
+            for i_s in intervals_steps:
+                for effort_step in range(i_s[0], i_s[1], i_s[2]):
+                    normalised_effort_step = effort_step * 100.0 / max_cost
+                    for tool in self.tools_results:
+                        if self.tools_results[tool].max_cost(metric) > i_s[0]:
+                            if tool in result:
+                                result[tool][normalised_effort_step] = self.tools_results[tool].mean_fd_at(effort_step, metric)
+                            else:
+                                result[tool] = {normalised_effort_step: self.tools_results[tool].mean_fd_at(effort_step, metric)}
+        if pickle_file is not None:
+            save_zipped_pickle(result, pickle_file)
+        return result
